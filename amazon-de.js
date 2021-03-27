faa4366925dfafd835c506a7879d4c182a90cce4
vilaleiva <vilaleiva@github.com>
2021-03-27 07:18:46 +0100

    amazon case added

---
+++ amazon-de.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer');
+const screenshot = "bahn-de.png";
+
+try{
+(async () => {
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+  await page.goto('https://www.amazon.de/');
+  await page.setViewport({
+    width: 1200,
+    height: 800
+});
+ 
+  await page.type("input#twotabsearchtextbox", "kaffeemaschine espresso");
+  await page.click("input#nav-search-submit-button")
+
+  // Amazon block bot testing like this. 
+
+ 
+})();
+} catch (err) {
+  console.error(err)
+}
